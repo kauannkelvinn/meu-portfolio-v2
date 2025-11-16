130da13236782b21e3d4a0a6c292a40c60fa6b95
unknown <kauankelvin7777@gmail.com>
2025-11-16 14:07:25 -0300

    feat: adicionar estrutura de layout base com Header e Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4">
-      <h1 className="font-matrix text-5xl">Olá, Neo.</h1>
-      <p>Este é um parágrafo normal na fonte Fira Code.</p>
+    <div className="flex flex-col items-center justify-center p-4">
+      <h1 className="font-matrix text-5xl text-matrix-light">Olá, Neo.</h1>
+      <p>
+        Este é um parágrafo normal na fonte Fira Code.
+      </p>
     </div>
   );
 }
