f5d875b95f6a6ca85c395087c6214145f8ec77c3
unknown <kauankelvin7777@gmail.com>
2025-11-16 11:57:49 -0300

    feat: setup inicial do Next.js 16+, Tailwind e fontes Matrix

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { VT323, Fira_Code } from "next/font/google";
+import "./globals.css";
+
+const firaCode = Fira_Code({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-fira-code",
+})
+
+const vt323 = VT323({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "400",
+  variable: "--font-vt323",
+})
+
+export const metadata: Metadata = {
+  title: "Portfólio Matrix",
+  description: "Portólio de Kauan Kelvin",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={`${firaCode.variable} ${vt323.variable} font-mono`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-4">
+      <h1 className="font-matrix text-5xl">Olá, Neo.</h1>
+      <p>Este é um parágrafo normal na fonte Fira Code.</p>
+    </div>
+  );
+}
